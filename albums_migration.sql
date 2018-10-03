f391591c1b071f7ceb64f26502217f7810b89a74
jameskevinayoung <james.kevin.a.young@gmail.com>
2018-10-03 14:25:06 -0500

    changed release date to an integer from date format

--- albums_migration.sql
+++ albums_migration.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS albums(
   id INT UNSIGNED NOT NULL AUTO_INCREMENT,
   artist  VARCHAR(50) DEFAULT 'NONE',
   name VARCHAR(200) NOT NULL,
-  release_date date ,
+  release_date int ,
   sales FLOAT ,
   genre VARCHAR(250),
   PRIMARY KEY(id)
