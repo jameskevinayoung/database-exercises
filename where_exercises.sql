65f576078c3b9425390abc358888a068aeb6e5f1
jameskevinayoung <james.kevin.a.young@gmail.com>
2018-10-04 14:40:46 -0500

    completed part 2 of 2 in where exercises

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,18 +1,31 @@
 USE employees;
 truncate employees;
-SELECT 'EMPLOYEES WITH FIRST NAMES IRENA, VIDYA, AND MAYA' AS 'QUERY SELECTION';
-SELECT DISTINCT emp_no, first_name, last_name FROM employees where first_name IN ('Irena', 'Vidya', 'Maya');
+SELECT 'EMPLOYEES WITH FIRST NAMES IRENA, VIDYA, AND MAYA' AS
+SELECT DISTINCT emp_no, first_name, last_name, gender
+FROM employees
+-- WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first
+WHERE gender = 'M'
+    AND first_name IN ('Irena', 'Vidya', 'Maya');
 
-SELECT 'EMPLOYEES WITH THE LAST NAMES THAT START WITH ''E''' AS 'QUERY SELECTION';
-SELECT DISTINCT emp_no, first_name, last_name FROM employees where last_name like 'E%';
+SELECT 'EMPLOYEES WITH THE LAST NAMES THAT START WITH ''E'''
+SELECT DISTINCT emp_no, first_name, last_name
+FROM employees
+WHERE last_name like 'E%' OR last_name like '%e';
 
 SELECT 'EMPLOYEES HIRED IN THE 1990S' AS 'QUERY SELECTION';
-SELECT emp_no, first_name, last_name FROM employees where hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+SELECT emp_no, first_name, last_name, hire_date, birth_date
+FROM employees
+wHERE hire_date  BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date like '%12-25';
 
 
 SELECT 'EMPLOYEES BORN ON CHRISTMAS' AS 'QUERY SELECTION';
-SELECT DISTINCT emp_no, first_name, last_name FROM employees where birth_date like '%12-25';
+SELECT DISTINCT emp_no, first_name, last_name
+FROM employees
+WHERE birth_date like '%12-25';
 
 
-SELECT 'EMPLOYESS WITH A ''Q'' IN THEIR LAST NAME' AS 'QUERY SELECION';
-SELECT DISTINCT emp_no, first_name, last_name FROM employees where last_name LIKE '%q%';
+SELECT 'EMPLOYESS WITH A ''Q'' IN THEIR LAST NAME' AS 'QUERY SELECTION'
+SELECT DISTINCT emp_no, first_name, last_name
+FROM employees
+WHERE last_name LIKE '%q%' and last_name not like '%qu%';
