a394e10312c1b5db7c6454f0992dfe4375075f9e
jameskevinayoung <james.kevin.a.young@gmail.com>
2018-10-04 14:58:52 -0500

    finished order by exercises

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -5,12 +5,18 @@ SELECT DISTINCT emp_no, first_name, last_name, gender
 FROM employees
 -- WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first
 WHERE gender = 'M'
-    AND first_name IN ('Irena', 'Vidya', 'Maya');
+    AND first_name IN ('Irena', 'Vidya', 'Maya')
+--     ORDER BY first_name, last_name ASC;
+    ORDER BY last_name, first_name ASC;
+
+
 
 SELECT 'EMPLOYEES WITH THE LAST NAMES THAT START WITH ''E'''AS 'QUERY SELECTION';
 SELECT DISTINCT emp_no, first_name, last_name
 FROM employees
-WHERE last_name like 'E%' OR last_name like '%e';
+WHERE last_name like 'E%' OR last_name like '%e'
+    ORDER BY emp_no desc ;
+
 
 SELECT 'EMPLOYEES HIRED IN THE 1990S' AS 'QUERY SELECTION';
 SELECT emp_no, first_name, last_name, hire_date, birth_date
@@ -22,7 +28,8 @@ AND birth_date like '%12-25';
 SELECT 'EMPLOYEES BORN ON CHRISTMAS' AS 'QUERY SELECTION';
 SELECT DISTINCT emp_no, first_name, last_name
 FROM employees
-WHERE birth_date like '%12-25';
+WHERE birth_date like '%12-25'
+ ORDER BY birth_date desc and hired_date asc;
 
 
 SELECT 'EMPLOYESS WITH A ''Q'' IN THEIR LAST NAME' AS 'QUERY SELECTION'
