f07ae42f08bcd90017319d271a1dd1f956b7d241
jameskevinayoung <james.kevin.a.young@gmail.com>
2018-10-08 16:56:30 -0500

    adlister application started

---
+++ adlister_application_db.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE adlister_application_db;
+
+create table users (
+    id INT unsigned NOT NULL AUTO_INCREMENT,
+    username VARCHAR(50),
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(16) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE ads (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    title varchar(250),
+    discription varchar(500),
+    category varchar(100),
+    user_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY(user_id) REFERENCES  users (id)
+
+);
